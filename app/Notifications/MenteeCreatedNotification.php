6a20dc0bd3981450058502afe8c452833f5158fb
oresomedude01-debug <oresomedude01@gmail.com>
2026-05-14 08:15:05 +0100

    mentee notifi

--- app/Notifications/MenteeCreatedNotification.php
+++ app/Notifications/MenteeCreatedNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class MenteeCreatedNotification extends Notification
+class MenteeCreatedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
